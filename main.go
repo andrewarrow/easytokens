6e0a298f544c8f0d417e0d5beced7821d4f0bf74
andrew arrow <oneone@gmail.com>
2016-08-07 10:43:05 -0400

    redirect

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "github.com/gin-gonic/gin"
 //import "net/http"
+import "os"
 
 func main() {
   fmt.Println("s")
@@ -13,6 +14,11 @@ func main() {
   r.GET("/", func(c *gin.Context) {
     data := gin.H{"nav_dash": true}
     c.HTML(200, "index.tmpl", data)
+  })  
+  r.POST("/auth", func(c *gin.Context) {
+    team := c.PostForm("team")
+    c.Redirect(http.StatusMovedPermanently, "https://" + team + 
+     ".slack.com/oauth/authorize?client_id=" + os.Getenv("EASY_CID") + "&scope=client")
   })  
   r.Run(":80") 
 }
